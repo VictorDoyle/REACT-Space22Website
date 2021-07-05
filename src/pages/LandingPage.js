eff2a4d9ea06705034e133af3dad9ef5f036771b
Victor Doyle <victor-doyle@outlook.com>
2021-07-05 17:29:10 +0200

    precommit

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,9 +1,26 @@
 import React from 'react';
-
+import "../styles/LandingPage.css"
+import { Link } from 'react-router-dom';
 function LandingPage() {
     return(
         <>
-        <h1> hello world </h1>
+        <h1 className="landingPageTitle"> Welcome To Space-22 Music </h1>
+
+        <div className="landingPageSelection">
+
+            <Link to="/albums">      
+            <button className="landingPageButtons">
+                Discover Albums
+            </button>
+            </Link>
+
+            <Link to="/learn-more">
+            <button className="landingPageButtons">
+                Learn About The Artist 
+            </button>
+            </Link>
+            
+        </div>
         </>
     )
 }
